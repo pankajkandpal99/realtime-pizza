7318f09613b7f4b8807dfb01257a943e3c93527b
pankaj kandpal <kandpalpankaj34@gmail.com>
2023-10-27 11:53:48 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const expressLayout = require('express-ejs-layouts');
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.render('home.ejs');
+})
+
+app.use(expressLayout);                           // Yeh line express-ejs-layouts middleware ko Express.js application me add kar rahi hai. Is middleware ka main kam layout-based templating system ko implement karna hai.
+app.set('views', path.join(__dirname, '/resources/views'));
+app.set('view engine', 'ejs');                    // Jab aap app.set('view engine', 'ejs') use karte hain, to Express.js automatic roop se EJS ko require aur use karta hai.
+
+app.listen(PORT, () => {
+    console.log(`server is listening on PORT ${PORT}.`);
+});
